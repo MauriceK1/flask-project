06908a30d43db2997e00e6d5388502eecd84d734
Maurice Korish <mauricekorish@gmail.com>
2022-06-26 18:02:13 -0400

    Redeploy-Site.Sh automation - updated to System Service Restart, as service created to launch Flask Portfolio Site on boot

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,12 +2,7 @@
 
 cd ~/
 
-pkill -f tmux
-cd flask-project
-git fetch && git reset origin/main --hard
+git fetch --all
+git pull
 
-
-tmux new-session -d -s site
-tmux send-keys source\  python3-virtualenv/bin/activate ENTER
-tmux send-keys pip \ install\  -r requirements.txt ENTER
-tmux send-keys flask \ run \ --host=0.0.0.0 ENTER
+sudo systemctl restart myportfolio
