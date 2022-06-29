9e58e7d586f8ca9fb5f847e8d36e9c3adc1c9e03
Maurice Korish <mauricekorish@gmail.com>
2022-06-29 17:13:39 -0400

    Final update 2 Redeploy-Site.sh for service - fixed directory

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cd /root/flask-project
+cd /root/flask-project/
 
 git fetch --all
 git pull
 
 source python3-virtualenv/bin/activate
-install -r  requirements.txt
+pip install -r requirements.txt
 
 sudo systemctl restart myportfolio
