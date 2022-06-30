976f07f58782c8b11a9b718a8c2f7a36b80d8a20
LinJing-Liu <linjingl93@gmail.com>
2022-06-30 11:09:45 -0400

    clean up files

--- mutation-test.sh
+++ mutation-test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 pip install mutpy
+# tests.test_db will create posts that show on the timeline page
 mut.py --target app.app --unit-test tests.test_db --runner unittest
 mut.py --target app.app --unit-test tests.test_app --runner unittest
--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-export FLASK_ENV=development
-flask run
